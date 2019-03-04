ae9de5e54192c377590a9030f3841083278facc5
Budenko <Budenko@competentum.ru>
2019-03-04 19:28:06 +0300

    Transform runtime issue

--- rollup.config.js
+++ rollup.config.js
@@ -10,33 +10,15 @@ export default [
 		output: {
 			name: 'howLongUntilLunch',
 			file: pkg.browser,
-			format: 'umd'
+			format: 'umd',
+			sourcemap:true
 		},
 		plugins: [
 			resolve(), // so Rollup can find `ms`
 			commonjs(), // so Rollup can convert `ms` to an ES module
 			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/main.js',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**']
+				      exclude: 'node_modules/**',
+				      runtimeHelpers: true
 			})
 		]
 	}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import ms from 'ms';
 import lunchtime from './lunchtime.js';
 import millisecondsUntil from './millisecondsUntil.js';
 
-export default function howLongUntilLunch(hours = 12, minutes = 30) {
+new Promise(resolve => resolve())
+		.then(_=> console.log('lol'));
+
+function howLongUntilLunch(hours = 12, minutes = 30) {
 	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
 	return ms(millisecondsUntilLunchTime, { long: true });
-} 
+}
+
+export {
+	howLongUntilLunch,
+	millisecondsUntil,
+	lunchtime
+}
